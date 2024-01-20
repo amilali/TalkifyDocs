a34c8216de4daee6a1b2e5662bdd4fcf897a5a2a
Amil Ali <amil13985@gmail.com>
2024-01-20 14:24:53 +0530

    Update Pinecone version and add getPineconeClient function

--- talkifydocs/src/lib/db/pinecone.ts
+++ talkifydocs/src/lib/db/pinecone.ts
@@ -1,1 +1,9 @@
-import 'dotenv/config';
+import { Pinecone, PineconeConfiguration } from '@pinecone-database/pinecone';
+
+export const getPineconeClient = () => {
+    return new Pinecone({
+        environment: process.env.NEXT_PUBLIC_PINECONE_ENVIRONMENT!,
+        apiKey: process.env.NEXT_PUBLIC_PINECONE_KEY!,
+    } as PineconeConfiguration);
+};
+
