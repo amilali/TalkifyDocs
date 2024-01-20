13682e5bdc18cd03dca8d4995881edbbd28e49ff
Amil Ali <amil13985@gmail.com>
2024-01-20 16:23:04 +0530

    Add langchain dependency and load S3 files into Pinecone

--- talkifydocs/src/lib/db/pinecone.ts
+++ talkifydocs/src/lib/db/pinecone.ts
@@ -1,4 +1,7 @@
+import aws from 'aws-sdk';
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf';
 import { Pinecone, PineconeConfiguration } from '@pinecone-database/pinecone';
+import { downloadFormS3 } from './s3-server';
 
 export const getPineconeClient = () => {
     return new Pinecone({
@@ -7,3 +10,7 @@ export const getPineconeClient = () => {
     } as PineconeConfiguration);
 };
 
+export async function loadS3IntoPinecone(fileKey:string){
+    console.log('downloading s3 files from system');
+    const file_name = await downloadFormS3(fileKey);    
+}
---
+++ talkifydocs/src/lib/db/s3-server.ts
@@ -0,0 +1,26 @@
+import AWS from 'aws-sdk'
+import fs from 'fs';
+export async function downloadFormS3(fileKey:string){
+    try {
+        AWS.config.update({
+            accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
+        });
+        const s3 = new AWS.S3({
+        params: { Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME },
+        region: 'ap-southeast-1',
+    });
+    const params = {
+        Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+        Key: fileKey,
+    };
+
+    const obj = await s3.getObject(params).promise();
+    const file_name = `/tmp/pdf-${Date.now()}.pdf`;
+    fs.writeFileSync(file_name, obj.Body as Buffer);
+    return file_name;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
